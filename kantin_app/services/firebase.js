c1d088aca6199c39416e85006cb8fcd21d31772a
Balazs Csutar <222855@buas.nl>
2024-09-09 22:13:58 +0200

    Bookmarks, recently viewed recepies

--- kantin_app/services/firebase.js
+++ kantin_app/services/firebase.js
@@ -5,13 +5,7 @@ import { getStorage } from 'firebase/storage';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtQZfUwc32jioTJ6WPRN8-rGiLxzfWI5A",
-  authDomain: "eten-57e5b.firebaseapp.com",
-  projectId: "eten-57e5b",
-  storageBucket: "eten-57e5b.appspot.com",
-  messagingSenderId: "760773061267",
-  appId: "1:760773061267:web:6fb891852100d445a83b66",
-  measurementId: "G-K6Y4HCEXP1"
+
 };
 
 // Initialize Firebase only once
